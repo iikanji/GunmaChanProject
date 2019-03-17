c93b2928aea74b888f8e5119423e43784eb7753d
SqueetleTB <39778430+SqueetleTB@users.noreply.github.com>
2019-03-16 17:06:27 -0700

    More clouds
    Added 3 more clouds
    Slightly decreased cloud move speed

--- core/src/asu/gunma/ui/util/BackgroundDrawer.java
+++ core/src/asu/gunma/ui/util/BackgroundDrawer.java
@@ -11,14 +11,20 @@ public class BackgroundDrawer {
     private Texture grass;
     private Texture mountain;
     private Texture sky;
-    private Texture cloud1;
-    private Texture cloud2;
+    private Texture cloudA1;
+    private Texture cloudA2;
+    private Texture cloudB1;
+    private Texture cloudB2;
+    private Texture cloudB3;
 
     private Random random;
     private double grassPosition;
     private double mountainPosition;
-    private double cloud1Position;
-    private double cloud2Position;
+    private double cloudA1Position;
+    private double cloudA2Position;
+    private double cloudB1Position;
+    private double cloudB2Position;
+    private double cloudB3Position;
 
     private final double CLOUD_X_MAX = 1024;
     private final double CLOUD_Y_MAX = 600;
@@ -28,14 +34,21 @@ public class BackgroundDrawer {
         this.grass = new Texture("background/grassx2.png");
         this.mountain = new Texture("background/mountainx3.png");
         this.sky = new Texture("background/skyx2.png");
-        this.cloud1 = new Texture("background/cloud1.png");
-        this.cloud2 = new Texture("background/cloud2.png");
+
+        this.cloudA1 = new Texture("background/cloud1.png");
+        this.cloudA2 = new Texture("background/cloud1.png");
+        this.cloudB1 = new Texture("background/cloud2.png");
+        this.cloudB2 = new Texture("background/cloud2.png");
+        this.cloudB3 = new Texture("background/cloud2.png");
 
         this.random = new Random();
         this.grassPosition = 0.0;
         this.mountainPosition = 300.0;
-        this.cloud1Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
-        this.cloud2Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
+        this.cloudA1Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
+        this.cloudA2Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
+        this.cloudB1Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
+        this.cloudB2Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
+        this.cloudB3Position = 0 + (CLOUD_X_MAX - 0) * random.nextDouble();
     }
 
     void show () {
@@ -46,8 +59,11 @@ public class BackgroundDrawer {
         if (isNotPaused && !isGameOver) {
             this.grassPosition -= 0.6;
             this.mountainPosition -= 0.020;
-            this.cloud1Position -= 0.2;
-            this.cloud2Position -= 0.2;
+            this.cloudA1Position -= 0.15;
+            this.cloudA2Position -= 0.15;
+            this.cloudB1Position -= 0.15;
+            this.cloudB2Position -= 0.15;
+            this.cloudB3Position -= 0.15;
         }
 
         this.batch.draw(this.sky,0, 0);
@@ -56,23 +72,38 @@ public class BackgroundDrawer {
         if (this.grassPosition < -600) {
             this.grassPosition = 0;
         }
-        if (this.cloud1Position < -300) {
-            this.cloud1Position = 1000;
+        if (this.cloudA1Position < -300) {
+            this.cloudA1Position = 1000;
         }
-        if (this.cloud2Position < -300) {
-            this.cloud2Position = 1000;
+        if (this.cloudA2Position < -300) {
+            this.cloudA2Position = 1000;
         }
-        this.batch.draw(this.cloud1, (int) this.cloud1Position, 340);
+        if (this.cloudB1Position < -300) {
+            this.cloudB1Position = 1000;
+        }
+        if (this.cloudB2Position < -300) {
+            this.cloudB2Position = 1000;
+        }
+        if (this.cloudB3Position < -300) {
+            this.cloudB3Position = 1000;
+        }
 
-        this.batch.draw(this.cloud2, (int) this.cloud2Position, 410);
+        this.batch.draw(this.cloudA1, (int) this.cloudA1Position, 340);
+        this.batch.draw(this.cloudA2, (int) this.cloudA2Position, 410);
+        this.batch.draw(this.cloudB1, (int) this.cloudB1Position, 410);
+        this.batch.draw(this.cloudB2, (int) this.cloudB2Position, 410);
+        this.batch.draw(this.cloudB3, (int) this.cloudB3Position, 410);
     }
 
     public void dispose() {
         this.grass.dispose();
         this.mountain.dispose();
         this.sky.dispose();
-        this.cloud1.dispose();
-        this.cloud2.dispose();
+        this.cloudA1.dispose();
+        this.cloudA2.dispose();
+        this.cloudB1.dispose();
+        this.cloudB2.dispose();
+        this.cloudB3.dispose();
     }
 
 }
